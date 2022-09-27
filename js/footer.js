f0f503e31037458446f874bd99348d8111d3a88f
Sergio Gimenez <gimenezsergio123@gmail.com>
2022-09-27 14:39:16 -0300

    Hicimos que el footer fuese dinamico

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+let miPie = function() {
+  let footer = /*html*/`
+	<div class="footer-bottom">
+    <h5>cfp27
+      perú 823 4to piso
+      capital federal
+      4362-1639
+      info@centro27.edu.ar</h5>
+  </div>
+  `
+  document.querySelector('footer').innerHTML = footer 
+}
+
+miPie()
+
+
